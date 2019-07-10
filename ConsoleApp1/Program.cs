29729638d4b7f1bcec53dbe6a274540b79a4e51f
John Doe <john.doe@gmail.com>
2019-07-10 10:23:27 -0300

    Compreendendo arrays lineares

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
@@ -10,7 +11,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("José Vitor");
+            string[] namelist = new string[2] {"Valor 1", "Valor 2" };
+
+            foreach (var item in namelist)
+                Console.WriteLine(item);
+
+            string[] dateList = new string[10000];
+            DateTime firstTime = DateTime.Now;
+
+            for (int i = 0; i < dateList.Length; i++)
+            {
+                dateList[i] = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fff");
+                Console.WriteLine(dateList[i]);
+            }
+
+            foreach (var item in dateList)
+                Console.WriteLine(item);
+
+            Console.WriteLine((DateTime.Now - firstTime).ToString());
+
             Console.ReadKey();
         }
     }
