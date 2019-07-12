78ec1bb25b7d9df6dde74152271c9013b7af8a6c
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-12 14:44:45 -0300

    Lista da receita do bolo completa.

---
+++ ReceitaDeBoloDeTapiocaCremoso/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceitaDeBoloDeTapiocaCremoso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BoloDeTapiocaCremoso();
+
+            Console.ReadKey();
+
+        }
+        /// <summary>
+        /// Metodo que mostra a receita do bolo de tapioca cremoso.
+        /// </summary>
+        public static void BoloDeTapiocaCremoso()
+        {
+            Console.WriteLine("                      Bem vindo as receitas HBSIS              ");
+            Console.WriteLine("                                                               ");
+            Console.WriteLine("           Bolo de tapioca cremoso (Não precisa ir ao forno)   ");
+            Console.WriteLine("\n\n\n            Tecle ENTER para seguir                     ");
+            Console.ReadKey();
+            Console.WriteLine("\n\n\n                       Ingredientes:                     ");
+            Console.WriteLine("\n\n- 500 g de tapioca granulada");
+            Console.WriteLine("\n- 2 xícaras (chá) de açúcar");
+            Console.WriteLine("\n- 1 litro de leite quente");
+            Console.WriteLine("\n- 200 g de coco ralado");
+            Console.WriteLine("\n- 1 vidro de leite de coco pequeno");
+            Console.WriteLine("\n- 1 lata de leite condensado");
+            Console.WriteLine("\n\n\n            Tecle ENTER para seguir                     ");
+            Console.ReadKey();
+            Console.WriteLine("\n\n1- Misture a tapioca e o açúcar, vá acrescendo aos poucos o leite quente.");
+            Console.WriteLine("\n\n              Tecle ENTER para seguir                     ");
+            Console.ReadKey();
+            Console.WriteLine("\n\n2 -  Quando começar a ficar cremoso, deixe descansar por 10 minutos, pois a tapioca vai crescer um pouco.");
+            Console.WriteLine("\n\n\n            Tecle ENTER para seguir                     ");
+            Console.ReadKey();
+            Console.WriteLine("\n\n3 - Após esse tempo, acrescente o coco ralado e o leite de coco, sempre mexendo bem.");
+            Console.WriteLine("\n\n\n            Tecle ENTER para seguir                     ");
+            Console.ReadKey();
+            Console.WriteLine("\n\n4 - Unte uma forma com leite condensado e coloque a massa, leve à geladeira por 2 horas.");
+            Console.WriteLine("\n\n\n            Tecle ENTER para seguir                     ");
+            Console.ReadKey();
+            Console.WriteLine("\n\n5 - Retire da geladeira e desenforme.");
+            Console.WriteLine("\n\n\n            Tecle ENTER para seguir                     ");
+            Console.ReadKey();
+            Console.WriteLine("\n\nAgora é so servir e aproveitar sua sobremesa que é uma delicia :*");
+            
+        }
+    }
+}
