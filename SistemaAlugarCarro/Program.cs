5c21e8071d10995efbce0fea653308350754f1b6
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-12 13:05:57 -0300

    Sistema de alugar e devolver carro finalizado!

--- SistemaAlugarCarro/Program.cs
+++ SistemaAlugarCarro/Program.cs
@@ -16,7 +16,7 @@ namespace SistemaAlugarCarro
            
             var opcaomenu = MenuPrincipal();
 
-            while (opcaomenu != 4)
+            while (opcaomenu != 3)
             {
                 if (opcaomenu == 1)
                     MenuAlugarCarro();
@@ -24,12 +24,10 @@ namespace SistemaAlugarCarro
                 if (opcaomenu == 2)
                     MenuDevolverCarro();
 
-                if (opcaomenu == 3)
-                    MenuDeSeguro();
-
-                MenuPrincipal();
+                opcaomenu = MenuPrincipal();
             }
 
+            
 
         }
         /// <summary>
@@ -59,8 +57,7 @@ namespace SistemaAlugarCarro
             Console.WriteLine("Como podemos ajuda-lo?");
             Console.WriteLine("\r\n1 - Alugar carro");
             Console.WriteLine("\r\n2 - Devolver carro");
-            Console.WriteLine("\r\n3 - Contratar seguro");
-            Console.WriteLine("\r\n4 - Sair do sistema");
+            Console.WriteLine("\r\n3 - Sair do sistema");
             Console.WriteLine("\r\nDigite a opção desejada:");
             Console.WriteLine("\r\n");
 
@@ -87,32 +84,46 @@ namespace SistemaAlugarCarro
         /// </summary>
         /// <param name="nomeCarro">Nome do carro a ser alugado.</param>
         /// <returns>Retorna verdadeiro caso o carro esteja disponivel para alugar.</returns>
-        public static bool PesquisaCarroParaAlugar(string nomeCarro)
+        public static bool? PesquisaCarroParaAlugar(string nomeCarro)
         {
             for (int i = 0; i < baseDeCarros.GetLength(0); i++)
             {
-                if (nomeCarro == baseDeCarros[i, 0])
+                if (CompararNomes(nomeCarro, baseDeCarros[i, 0]))
                 {
                     Console.WriteLine($"O Carro: {nomeCarro}" +
-                          $" pode ser alocado?: {baseDeCarros[i, 2]}");
+                          $" pode ser alugado?: {baseDeCarros[i, 2]}");
 
                     return baseDeCarros[i, 2] == "sim";
                 }
             }
-            return false;
+
+            Console.WriteLine("Nenhum carro encontrado. Deseja realizar a busca novamente ?");
+            Console.WriteLine("Digite o número da opção desejada: Sim - 1     Não - 2");
+
+            int.TryParse(Console.ReadKey().KeyChar.ToString(), out int opcao);
+
+            if (opcao == 1)
+            {
+                Console.WriteLine("Digite o nome do carro a ser pesquisado:");
+                nomeCarro = Console.ReadLine();
+
+                return PesquisaCarroParaAlugar(nomeCarro);
+            }
+
+            return null;
         }
         /// <summary>
         /// Metodo para alterar a informação de alugar um carro.
         /// </summary>
-        /// <param name="nomeCarro">Nome do carro.</param>
+        /// <param name="nomeDeCarro">Nome do carro.</param>
         /// <param name="alugar">Parametro que diz se o carro pode ou não ser alugado.</param>
         public static void AtualizarCarro(string nomeCarro, bool alugar)
         {
             for (int i = 0; i < baseDeCarros.GetLength(0); i++)
             {
-                if (nomeCarro == baseDeCarros[i, 0])
+                if (CompararNomes(nomeCarro, baseDeCarros[i, 0]))
                 {
-                    baseDeCarros[i, 2] = alugar ? "não" : "sim";
+                    baseDeCarros[i, 2] = alugar? "não" : "sim";
                 }
             }
 
@@ -124,18 +135,26 @@ namespace SistemaAlugarCarro
         {
             MostrarMenuInicialCarros("MenuAlugarCarro um carro");
 
-            var nomeCarro = Console.ReadLine();
-            if (PesquisaCarroParaAlugar(nomeCarro))
+            var nomedoCarro = Console.ReadLine();
+            var resultadoPesquisa = PesquisaCarroParaAlugar(nomedoCarro);
+
+            if (resultadoPesquisa != null && resultadoPesquisa == true)
             {
                 Console.Clear();
                 BemVindo();
                 Console.WriteLine("\n\nVocê deseja alugar esse carro? para sim - 1  para não - 2");
 
-                AtualizarCarro(nomeCarro, Console.ReadKey().KeyChar.ToString() == "1");
+                AtualizarCarro(nomedoCarro, Console.ReadKey().KeyChar.ToString() == "1");
 
                 MostarListaDeCarros();
 
                 Console.ReadKey();
+            }
+
+            if (resultadoPesquisa == null)
+            {
+                Console.WriteLine("Nenhum livro encontrado em nossa base de dados do sistema.");
+
             }
         }
         /// <summary>
@@ -144,18 +163,28 @@ namespace SistemaAlugarCarro
         public static void MenuDevolverCarro()
         {
             MostrarMenuInicialCarros("Devolver um carro");
+
+            MostarListaDeCarros();
+
             var nomeCarro = Console.ReadLine();
-            if (PesquisaCarroParaAlugar(nomeCarro))
+            var resultadoPesquisa = PesquisaCarroParaAlugar(nomeCarro);
+
+            if (resultadoPesquisa != null && resultadoPesquisa == false)
             {
                 Console.Clear();
                 BemVindo();
-                Console.WriteLine("\n\nVocê deseja alugar esse carro? para sim - 1  para não - 2");
+                Console.WriteLine("\n\nVocê deseja devolver esse carro? para sim - 1  para não - 2");
 
                 AtualizarCarro(nomeCarro, Console.ReadKey().KeyChar.ToString() == "0");
 
                 MostarListaDeCarros();
 
                 Console.ReadKey();
+            }
+
+            if (resultadoPesquisa == null)
+            {
+                Console.WriteLine("Nenhum livro encontrado em nossa base de dados do sistema.");
             }
 
 
@@ -165,18 +194,12 @@ namespace SistemaAlugarCarro
         /// </summary>
         public static void MostarListaDeCarros()
         {
-            Console.Clear();
-            BemVindo();
-
             Console.WriteLine("Carros ainda disponiveis:");
 
             for (int i = 0; i < baseDeCarros.GetLength(0); i++)
             {
                 Console.WriteLine($"\nNome: {baseDeCarros[i, 0]} Ano: {baseDeCarros[i, 1]} Disponivel: {baseDeCarros[i, 2]}");
             }
-            Console.ReadKey();
-
-
 
         }
         /// <summary>
@@ -192,26 +215,18 @@ namespace SistemaAlugarCarro
             Console.WriteLine($"Menu - {operacao}");
             Console.WriteLine("Digite o nome do carro para realizar a operação:");
         }
-
-        public static void MenuDeSeguro()
+        /// <summary>
+        /// Metodo que compara duas string deixando em caixa alta e removendo espaços vazios dentro da mesma.
+        /// </summary>
+        /// <param name="primeiro">Primeira string.</param>
+        /// <param name="segundo">Segunda string.</param>
+        /// <returns></returns>
+        public static bool CompararNomes(string primeiro, string segundo)
         {
-            Console.Clear();
-            BemVindo();
+            if (primeiro.ToUpper().Replace(" ","") == segundo.ToUpper().Replace(" ",""))
+                return true;
 
-            Console.WriteLine("\n\n           Escolha uma opção:");
-            Console.WriteLine("\n\n1 - Seguro completo: R$500,00");
-            Console.WriteLine("2 - Seguro parcial: R$350,00");
-            Console.WriteLine("3 - Voltar");
-            Console.WriteLine("\n\nDigite a opção desejada");
-
-            var opcaoDeSeguro = Console.ReadLine();
-
-            while (opcaoDeSeguro != 3)
-            {
-
-            }
-            }
-
+            return false;
         }
 
         
