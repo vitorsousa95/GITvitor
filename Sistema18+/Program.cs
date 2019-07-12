55ab781a3c6546c027f87e5a24f570c7d992a44f
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-12 13:35:14 -0300

    Sistema onde o usuario pode se identicar como maior ou menor de 18 anos.

---
+++ Sistema18+/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema18_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma opção:");
+            Console.WriteLine("(1) para menor de 18 \n(2) para maior de 18");
+
+            int.TryParse(Console.ReadKey().KeyChar.ToString(), out int opcao);
+
+            if (opcao == 1)
+            {
+                Console.WriteLine("\n\nVocê não pode entrar,nem beber! Volte para sua casa.");
+            }
+
+            if (opcao == 2)
+                {
+                    Console.WriteLine("\n\nVocê pode entrar, chapar e ficar doidão (*_*)");
+                }
+
+
+            Console.ReadKey();
+        }
+    }
+}
