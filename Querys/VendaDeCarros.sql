8d01e3b04ed4efb3e20f15678047f4ec6461af35
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-17 17:02:10 -0300

    up

---
+++ Querys/VendaDeCarros.sql
@@ -0,0 +1,12 @@
+﻿select 'Carros' as 'Carros',
+		SUM(Temp.[Media de Vendas]) as 'Vendas',
+		Temp.[Vendas no Ano]
+from(select 
+		ModeloDoCarro,
+		Ano,
+		(SUM(NumeroDeVendas)/ COUNT(*)) as 'Media de Vendas',
+		YEAR(DataDaVenda) as 'Vendas no Ano'
+		from TabelaDeCarros
+	GROUP BY ModeloDoCarro,Ano, YEAR(DataDaVenda)) Temp
+group by Temp.[Vendas no Ano]
+	
