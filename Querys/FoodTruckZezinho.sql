e90e29d0e4bac829f9d60d2257507324a716d67c
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-17 17:28:52 -0300

    Food Truck Vendas

---
+++ Querys/FoodTruckZezinho.sql
@@ -0,0 +1,8 @@
+﻿select
+		Tipodog,
+		TotalDeVendas,
+		SUM(TotalDeVendas)  as 'VendasTotalMês',
+		MONTH(DataVenda) as 'Mês'
+		from FoodTruckDoZezinho
+		group by Tipodog,TotalDeVendas,MONTH(DataVenda)
+		order by TotalDeVendas asc
