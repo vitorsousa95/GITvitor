553cb3c17c16462d2e41fe011860f80be487ea2e
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-17 11:58:39 -0300

    exercicio da aula

---
+++ Querys/MyFirstInsrtInto.sql
@@ -0,0 +1,23 @@
+﻿/*insert into AulaCsharp
+(Aluno,Idade, Ativo, UsuInc, UsuAlt,DatInc, DatAlt)
+values
+('Vitor', 23, 1, 1, 1, GETDATE(), GETDATE())
+go
+select * from AulaCsharp*/
+
+/*select
+		Aluno,
+		Idade,
+		IIF(Ativo = 1, 'Ativo','inativo') as 'Status'
+	from AulaCsharp
+where Id in
+		(select Id
+			from AulaCsharp 
+			where Idade > 20)
+order by Id desc*/
+
+
+update AulaCsharp
+	set Aluno = 'Master'
+	where Id = 1
+	select * from AulaCsharp
