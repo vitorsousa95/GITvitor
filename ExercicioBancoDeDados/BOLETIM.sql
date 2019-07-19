de5dcc743a697a6178641803bebe3039f7755023
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-19 12:01:22 -0300

    up

---
+++ ExercicioBancoDeDados/BOLETIM.sql
@@ -0,0 +1,12 @@
+﻿select
+		AL.Aluno,
+		AL.Turma,
+		(NT.Nota1 + NT.Nota2 + NT.Nota3 + NT.Nota4)/4 as 'Media',
+
+		IIF (((NT.Nota1 + NT.Nota2 + NT.Nota3 + NT.Nota4)/4 >= 7 and ), 'Aprovado','Reprovado') as 'Status'
+
+	from Alunos AL
+	inner join Notas NT on AL.Id = NT.Aluno
+	inner join Frequencias FQ on AL.Id = FQ.Aluno
+
+	
