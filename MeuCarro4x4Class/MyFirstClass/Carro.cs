bf515641f9a804eed011096c6a5d8cb5a2fd0fdd
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-23 17:27:51 -0300

    up

---
+++ MeuCarro4x4Class/MyFirstClass/Carro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuCarro4x4Class.MyFirstClass
+{
+    class Carro
+    {
+        public int Rodas { get; set; } = 0;
+        public int Portas { get; set; } = 0;
+        public int Bancos { get; set; } = 0;
+        public int Janelas { get; set; } = 0;
+        
+
+        public void MostrarCarro()
+        {
+            Console.WriteLine($"Nosso carro:\r\n" +
+                $"Quant Rodas[{Rodas}]\r\n" +
+                $"Quant Portas[{Portas}]\r\n" +
+                $"Quant Bancos[{Bancos}]\r\n" +
+                $"Quant Janelas[{Janelas}]");
+        }
+    }
+}
