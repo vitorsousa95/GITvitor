ddfdaaa0cac910b0276572f019e439a50e248ebe
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-24 17:18:33 -0300

    Ciando bliblioteca de calculadora e usando ela como referencia em outro projeto

---
+++ BibliotecaCalculadora/BibliotecaCalculadora/Calculadora.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaCalculadora
+{
+    public class Calculadora
+    {
+        public int Soma(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Subtrai(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Divide(int x, int y)
+        {
+            return x / y;
+        }
+
+        public int Multiplica(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ UsandoBibliotecaCalculadora/Program.cs
@@ -0,0 +1,26 @@
+﻿using BibliotecaCalculadora;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsandoBibliotecaCalculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calcular = new Calculadora();
+            Console.WriteLine("Soma");
+            Console.WriteLine(calcular.Soma(10, 10));
+            Console.WriteLine("Subtrai");
+            Console.WriteLine(calcular.Subtrai(10, 10));
+            Console.WriteLine("Divide");
+            Console.WriteLine(calcular.Divide(10, 10));
+            Console.WriteLine("Multiplica");
+            Console.WriteLine(calcular.Multiplica(10, 10));
+            Console.ReadKey();
+        }
+    }
+}
