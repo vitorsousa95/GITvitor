fc5560073e71be505f48c8ba9dcf2cb026727f57
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-29 09:28:34 -0300

    orientação objeto, exemplo de encapsulamento

---
+++ BancoCeSharp/Classes/Conta.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoCeSharp.Classes
+{
+    public class Conta
+    {
+        double saldo = 0;
+        /// <summary>
+        /// propriedade que contem o valor do saldo
+        /// </summary>
+        public double Saldo { get { return saldo; } }
+
+        public Conta()
+        {
+            //Bonus de mil reais para iniciar operar daytrade
+            saldo = 1000;
+        }
+
+        /// <summary>
+        /// Metodo para sacar de acordo com saldo disponivel
+        /// </summary>
+        /// <param name="valor">Valor a ser sacado</param>
+        /// <returns>retorna se o saque foi feito ou não</returns>
+        public bool Sacar(double valor)
+        {
+            if(valor <= saldo)
+            {//desconta do valor em saldo na conta
+                saldo -= valor;
+                return true;
+            }
+
+            return false;
+        }
+        /// <summary>
+        /// Metodo para mostrar saldo em conta que está disponivel
+        /// </summary>
+        public double MostrarSaldo()
+        {
+            return saldo;
+        }
+    }
+}
---
+++ BancoCeSharp/Program.cs
@@ -0,0 +1,30 @@
+﻿using BancoCeSharp.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoCeSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta acconuntUser = new Conta();
+
+            Console.WriteLine("Informe o valor a ser sacado:");
+            //Aqui usamos a conversão do valor informado
+            double.TryParse(Console.ReadLine(), out double valorSacar);
+            //Verficamos se o valor é maior que 0 e se realizou o saque
+            if(valorSacar > 0 && acconuntUser.Sacar(valorSacar))
+                Console.WriteLine("Saque realizado com sucesso");
+            else
+                Console.WriteLine("Operação não realizada");
+            //Mostra o saldo disponivel
+            Console.WriteLine($"Saldo atual disponivel:{acconuntUser.Saldo.ToString("N2")}");
+
+            Console.ReadKey();
+        }
+    }
+}
