f57d535b783009c9d1d6535a16006be7578e0211
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-29 11:56:17 -0300

    exercicio criar classe de emcapsulamento

---
+++ CantinaGG/Classes/Compra.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CantinaGG.Classes
+{
+    public class Compra
+    {
+        public string[,] listaLanches;
+        double saldoZezinho = 0;
+
+        public double Saldo  { get { return saldoZezinho; } }
+
+        public Compra()
+        {
+            saldoZezinho = 20;
+            LitaDeLanches();
+        }
+
+        public bool CompraLanche(double preço)
+        {
+            if (preço <= saldoZezinho)
+            {
+                saldoZezinho -= preço;
+                return true;
+            }
+            return false;
+        }
+
+        public void LitaDeLanches()
+        {
+            listaLanches = new string[2, 2]
+            {
+                {"Doguinho","2,00" },
+                {"X-Salada","6,00" }
+            };
+        }
+    }
+}
---
+++ CantinaGG/Program.cs
@@ -0,0 +1,45 @@
+﻿using CantinaGG.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CantinaGG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Compra sourico = new Compra();
+            
+
+            Console.WriteLine("Qual lanche deseja comprar hoje Zezinho?");
+            Console.WriteLine("1 - Doguinho");
+            Console.WriteLine("2 - X-Salada");
+            Console.WriteLine("Digite o numero do lanche que ");
+            int.TryParse(Console.ReadLine(), out int opcao);
+
+            if (opcao == 1)
+            {
+                Console.WriteLine("Compra realizada com sucesso!");
+                sourico.CompraLanche(double.Parse(sourico.listaLanches[0, 1].ToString()));
+                Console.WriteLine($"Seu novo saldo é:R${sourico.Saldo.ToString("N2")}");
+            }
+            if (opcao == 2)
+            {
+                Console.WriteLine("Compra realizada com sucesso!");
+                sourico.CompraLanche(double.Parse(sourico.listaLanches[1, 1].ToString()));
+                Console.WriteLine($"Seu novo saldo é:R${sourico.Saldo.ToString("N2")}");
+            }
+            if (opcao != 1 && opcao != 2)
+            {
+                Console.WriteLine("Infelismente você escolheu a opção errada!");
+            }
+
+            Console.ReadKey();
+            
+        }
+        
+    }
+}
